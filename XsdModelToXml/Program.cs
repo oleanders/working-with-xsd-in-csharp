ad7194e0d286a14334e4336dce0aafeaa647195a
oae <oae@vaf.no>
2016-11-08 14:33:39 +0100

    .

--- XsdModelToXml/Program.cs
+++ XsdModelToXml/Program.cs
@@ -16,12 +16,18 @@ namespace XsdModelToXml
 
             var arbeidstaker = new ArbeidstakerType
             {
+                identifikatorer = new IdentifikatorType[]
+                {
+                  new IdentifikatorType() { identifikatortype = "FNR", identifikatorverdi = "12335435345" },
+                  new IdentifikatorType() { identifikatortype = "RESNR", identifikatorverdi = "12335" }
+                },
                 fulltNavn = "Ole Olsen",
                 navn = new PersonnavnType()
                 {
                     fornavn = "Ole",
                     etternavn = "Olsen"
                 }
+
             };
             
             // 1. xml serialization
