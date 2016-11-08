ee6585ff65375f465066b381ed658c72b2adf992
oae <oae@vaf.no>
2016-11-08 11:24:20 +0100

    added ps update model script

---
+++ XsdModelToXml/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XsdModelToXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var arbeidstaker = new ArbeidstakerType
+            {
+                fulltNavn = "Ole Olsen",
+                navn = new PersonnavnType()
+                {
+                    fornavn = "Ole",
+                    etternavn = "Olsen"
+                }
+            };
+            
+            // 1. xml serialization
+            var serializer = new XmlSerializer(typeof(ArbeidstakerType));
+            using (var writer = new StreamWriter(@"..\..\Models\ExampleXml\arbeidstaker-using-xml-serialization.xml"))
+            {
+                serializer.Serialize(writer, arbeidstaker);
+            }
+
+            // 2. data contract serialization
+            var serializer2 = new DataContractSerializer(typeof(ArbeidstakerType));
+            using (FileStream writer = new FileStream(@"..\..\Models\ExampleXml\arbeidstaker-using-data-contract-serialization.xml", FileMode.Create))
+            {
+                serializer2.WriteObject(writer, arbeidstaker);
+            }
+        }
+    }
+}
---
+++ XsdModelToXml/UpdateModel.ps1
@@ -0,0 +1,16 @@
+﻿$xsdFolder = "Models\Xsd\"
+$xsds = @(
+    'https://raw.githubusercontent.com/FINTprosjektet/fint-informasjonsmodell/master/xsd/1.0/FINT/Felles/fintfelles.xsd',
+    'https://raw.githubusercontent.com/FINTprosjektet/fint-informasjonsmodell/master/xsd/1.0/FINT/Arbeidstaker/arbeidstaker.xsd'
+)
+$xsdTool = "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\xsd.exe"
+
+# DOWNLOAD FILES TO XSD FOLDER
+foreach($xsd in $xsds){
+    $filename = [System.IO.Path]::GetFileName($xsd);
+    (New-Object Net.WebClient).DownloadFile($xsd, $xsdFolder + $filename)
+}
+
+# GENERATE CS-file from XSD
+$args = @('/classes', "/o:$xsdFolder..")
+& $xsdTool ((Get-ChildItem $xsdFolder | ForEach-Object { $xsdFolder + $_.Name }) + $args)
